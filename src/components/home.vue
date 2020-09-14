c3998d6c74e5af5259134380a2aadbe0e253d613
ZhouLiangLiangJS <1104117485@qq.com>
2020-09-14 18:11:00 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <Navmenu :active="0"></Navmenu>
+    <div class="main">
+      <div class="main_box">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navmenu from "./navmenu"
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components:{
+      Navmenu
+    }
+  }
+</script>
+
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+  .main{
+    width: 84.91vw;
+    height: 100vh;
+    background: #F5F5F5 0% 0% no-repeat padding-box;
+    float: left;
+    position: relative;
+  }
+  .main_box{
+    width: 90%;
+    height: 90%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
+</style>
---
+++ src/components/navmenu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="navmenu">
+    <div class="navmenu-nav">沧县职教中心 <br>迎新系统</div>
+    <div class="navmenu-icon">
+      <img src="../../static/icon2.jpg" >
+      <p>管理员</p>
+    </div>
+    <div class="navmenu-list">
+      <div v-for="(item,i) in arrlist" :style="active==i?'color: rgb(245, 222, 62);':''">{{item}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        arrlist:["新生一览","教师列表","缴费管理","班级管理","宿舍管理","发送通知","常见问题","系统管理"]
+      }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    props:["active"]
+  }
+</script>
+<style scoped>
+
+  .navmenu{
+    width: 15.09vw;
+    height: 100vh;
+    background-color: #272E36;
+    position: relative;
+    color: #FFFFFF;
+    font-size: 14px;
+    float: left;
+  }
+  .navmenu-nav{
+    top: 5vh;
+    width: 210px;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 95px;
+    position: absolute;
+    font-size: 25px;
+    color: var(--unnamed-color-ffffff);
+    text-align: center;
+    font-weight: 900;
+    text-shadow: 0px 3px 10px #FFFFFF29;
+    opacity: 1;
+  }
+  .navmenu-icon{
+    top: 21vh;
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 1;
+    position: absolute;
+    text-align: center;
+  }
+  .navmenu-icon>img{
+    width: 30px;
+  }
+  .navmenu-icon>p{
+    margin-top: 0;
+  }
+  .navmenu-list{
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 35vh;
+  }
+  .navmenu-list>div{
+    letter-spacing:10px;
+    width: 104px;
+    border-bottom: 1px solid #707070;
+    line-height: 7vh;
+    text-align: center;
+    padding-left: 6px;
+    cursor:pointer
+  }
+</style>
